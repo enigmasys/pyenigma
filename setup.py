07985bbbf3b19a4c8b5e1673dbce9ce9200092fa
yogeshvu <yogesh.d.barve@vanderbilt.edu>
2023-11-16 14:04:33 -0600

    update the setup.py

--- setup.py
+++ setup.py
@@ -2,15 +2,25 @@
 # Path: setup.py
 from setuptools import setup, find_packages
 
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+
 setup(
     name="pyudcp",
-    version="0.0.8",
+    version="0.2.0",
     description="Python binding for Enigma CLI/ UDCP CLI",
     author="yogeshvu",
     author_email="yogesh.d.barve@vanderbilt.edu",
+    url="https://github.com/enigmasys/pyenigma",
     packages=find_packages(),
     install_requires= ["datamodel-code-generator", "pydantic~=2.4.2"],
     python_requires=">=3.7",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+
+
     # entry_points={
     #     "console_scripts": [
     #         "pyudcp = src.pyudcp.enigmapy:main",
