07d5afeb07d457d190f43c9c06a0a4e96fb25e23
yogeshvu <yogesh.d.barve@vanderbilt.edu>
2023-12-13 09:43:09 -0600

    added the MANIFEST.in and increment the version

--- setup.py
+++ setup.py
@@ -7,14 +7,18 @@ this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
 
 
+# Also include the readme.md file in the package
+
+
 setup(
     name="pyudcp",
-    version="0.2.0",
+    version="0.3.0",
     description="Python binding for Enigma CLI/ UDCP CLI",
     author="yogeshvu",
     author_email="yogesh.d.barve@vanderbilt.edu",
     url="https://github.com/enigmasys/pyenigma",
     packages=find_packages(),
+    include_package_data=True,
     install_requires= ["datamodel-code-generator", "pydantic~=2.4.2"],
     python_requires=">=3.7",
     long_description=long_description,
